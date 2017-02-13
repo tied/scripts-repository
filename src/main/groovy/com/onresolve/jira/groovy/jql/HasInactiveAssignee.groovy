5234236e30446ac6d3703acd60b7d089f262737c
Michael <michael.r808@gmail.com>
2017-02-12 16:32:14 -0800

    commit

--- src/main/groovy/com/onresolve/jira/groovy/jql/HasInactiveAssignee.groovy
+++ src/main/groovy/com/onresolve/jira/groovy/jql/HasInactiveAssignee.groovy
@@ -1,37 +1,19 @@
 package com.onresolve.jira.groovy.jql
 
-import com.atlassian.jira.ComponentManager
 import com.atlassian.jira.component.ComponentAccessor
 import com.atlassian.jira.issue.Issue
-import com.atlassian.jira.issue.search.SearchProvider
-import com.atlassian.jira.jql.operand.QueryLiteral
-import com.atlassian.jira.jql.parser.JqlQueryParser
-import com.atlassian.jira.jql.query.RangeQueryFactory
-import com.atlassian.jira.util.MessageSetImpl
-import com.atlassian.jira.web.bean.PagerFilter
-import com.atlassian.query.operand.FunctionOperand
-import com.atlassian.query.operator.Operator
-import org.apache.log4j.Category
-import com.atlassian.jira.util.MessageSet
-import com.atlassian.crowd.embedded.api.User
-import com.atlassian.query.clause.TerminalClause
 import com.atlassian.jira.jql.query.QueryCreationContext
-import org.apache.lucene.document.NumberTools
+import com.atlassian.jira.security.JiraAuthenticationContext
+import com.atlassian.jira.user.ApplicationUser
+import com.atlassian.jira.util.MessageSet
+import com.atlassian.jira.util.MessageSetImpl
+import com.atlassian.query.clause.TerminalClause
+import com.atlassian.query.operand.FunctionOperand
 import org.apache.lucene.index.Term
-import org.apache.lucene.search.Query
-import org.apache.lucene.search.TermQuery
-import com.onresolve.jira.groovy.jql.AbstractScriptedJqlFunction
 import org.apache.lucene.search.BooleanClause
 import org.apache.lucene.search.BooleanQuery
 import org.apache.lucene.search.Query
 import org.apache.lucene.search.TermQuery
-import com.atlassian.jira.issue.fields.CustomField
-import com.atlassian.jira.issue.CustomFieldManager
-import com.atlassian.jira.component.ComponentAccessor
-import com.atlassian.jira.user.ApplicationUser;
-import com.atlassian.jira.security.JiraAuthenticationContext;
-
-
 
 class HasInactiveAssignee extends AbstractScriptedJqlFunction implements JqlQueryFunction{
     @Override
@@ -56,16 +38,15 @@ class HasInactiveAssignee extends AbstractScriptedJqlFunction implements JqlQuer
 
     def String subquery
     //@Override
-    MessageSet validate(User user, FunctionOperand operand, TerminalClause terminalClause) {
+    MessageSet validate(ApplicationUser user, FunctionOperand operand, TerminalClause terminalClause) {
         def messageSet = new MessageSetImpl()
         return messageSet
     }
 
     @Override
     Query getQuery(QueryCreationContext queryCreationContext, FunctionOperand operand, TerminalClause terminalClause) {
-        //User user=ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser();
         JiraAuthenticationContext context = ComponentAccessor.getJiraAuthenticationContext();
-        ApplicationUser applicationUser = context.getUser();
+        ApplicationUser applicationUser = context.getLoggedInUser();
         def booleanQuery = new BooleanQuery()
         issues = getIssues(operand.args[0], applicationUser)
         issues.each {Issue issue ->
--- src/main/groovy/com/onresolve/jira/groovy/jql/Test.groovy
+++ src/main/groovy/com/onresolve/jira/groovy/jql/Test.groovy
@@ -1,18 +1,21 @@
 
 package com.onresolve.jira.groovy.jql
 
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.Issue
 import com.atlassian.jira.jql.query.QueryCreationContext
+import com.atlassian.jira.security.JiraAuthenticationContext
+import com.atlassian.jira.user.ApplicationUser
+import com.atlassian.jira.util.MessageSet
+import com.atlassian.jira.util.MessageSetImpl
 import com.atlassian.query.clause.TerminalClause
 import com.atlassian.query.operand.FunctionOperand
-import com.atlassian.jira.user.ApplicationUser
-import groovy.util.logging.Log4j
+import org.apache.lucene.index.Term
+import org.apache.lucene.search.BooleanClause
+import org.apache.lucene.search.BooleanQuery
 import org.apache.lucene.search.Query
-import com.atlassian.jira.util.*
-import org.apache.lucene.queryParser.QueryParser
+import org.apache.lucene.search.TermQuery
 
-import java.text.MessageFormat
-
-@Log4j
 class Test extends AbstractScriptedJqlFunction implements JqlQueryFunction {
     @Override
     String getDescription() {
@@ -34,7 +37,6 @@ class Test extends AbstractScriptedJqlFunction implements JqlQueryFunction {
         "_testFunction"
     }
 
-    def String subquery
     @Override
     MessageSet validate(ApplicationUser user, FunctionOperand operand, TerminalClause terminalClause) {
         def messageSet = new MessageSetImpl()
@@ -43,9 +45,18 @@ class Test extends AbstractScriptedJqlFunction implements JqlQueryFunction {
 
     @Override
     Query getQuery(QueryCreationContext queryCreationContext, FunctionOperand operand, TerminalClause terminalClause) {
+        JiraAuthenticationContext context = ComponentAccessor.getJiraAuthenticationContext()
+        ApplicationUser applicationUser = context.getLoggedInUser()
+        def booleanQuery = new BooleanQuery()
+        issues = getIssues(operand.args[0], applicationUser)
+        issues.each {Issue issue ->
+            try {
+                booleanQuery.add(new TermQuery(new Term("issue_id", issue.id as String)), BooleanClause.Occur.SHOULD)
+            } catch (NullPointerException NPE) {
 
-        MessageFormat queryStr = new MessageFormat("project = scrum")
-        def query = queryParser.parseQuery(queryStr)
-//        luceneQueryBuilder.createLuceneQuery(queryCreationContext, query.whereClause)
+            }
+        }
+
+        return booleanQuery
     }
 }
